23033f2c559382b123f08f9c6fd1ad8ecbf0b56d
Marc Christensen <mecworks@gmail.com>
2010-09-01 16:54:52 -0600

    Fixed paths in exec_push.sh and removed lame editor backup files.

--- rank server/exec_push.sh
+++ rank server/exec_push.sh
@@ -4,11 +4,11 @@ OUTPUT_DIR="/home/rupert/rank/rank server/Output"
 GET_RANK_DIR="/home/rupert/rank/rank server"
 
 echo "  ***  Running getRank.sh"
-${GET_RANK_DIR}/getRank.sh -d "${OUTPUT_DIR}" -g "${GIT_DIR}"
+"${GET_RANK_DIR}"/getRank.sh -d "${OUTPUT_DIR}" -g "${GIT_DIR}"
 echo "  ***  Done"
 
-cd ${OUTPUT_DIR}
+sleep 5
 find -iname "*~" | xargs -i rm -rv "{}"
 echo "  ***  Pushing rank files to remote site"
-scp -r ${OUTPUT_DIR}/* mono-web@go-mono.com:go-mono/rank
+scp -r "${OUTPUT_DIR}"/* mono-web@go-mono.com:go-mono/rank
 echo "  ***  Done"
