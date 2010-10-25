73e93cad9558373a41a3dfae2428a5978da8eb7f
David Mulder <dmulder@novell.com>
2010-10-25 13:19:51 -0600

    Modified exec_push so only one script is needed.

--- rank server/exec_push.sh
+++ rank server/exec_push.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 GIT_DIR="/home/rupert/monogit"
 OUTPUT_DIR="/home/rupert/rank/rank server/Output"
-GET_RANK_DIR="/home/rupert/rank/rank server"
+GET_RANK_DIR="/home/rupert/rank/getRank/bin/Release/"
 
-echo "  ***  Running getRank.sh"
-"${GET_RANK_DIR}"/getRank.sh -d "${OUTPUT_DIR}" -g "${GIT_DIR}"
+echo "  ***  Running getRank.exe"
+mono "${GET_RANK_DIR}"/getRank.exe -d "${OUTPUT_DIR}" -g "${GIT_DIR}"
 echo "  ***  Done"
 
 sleep 5
@@ -13,3 +13,4 @@ find -iname "*~" | xargs -i rm -rv "{}"
 echo "  ***  Pushing rank files to remote site"
 scp -r "${OUTPUT_DIR}"/* mono-web@go-mono.com:go-mono/bananas
 echo "  ***  Done"
+
