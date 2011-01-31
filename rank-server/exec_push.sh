87e75ed287176cdcc72c767e1f0baed0ab6f77bc
David Mulder <dmulder@novell.com>
2011-01-31 12:13:41 -0800

    Set locale variables in cron script.

--- rank-server/exec_push.sh
+++ rank-server/exec_push.sh
@@ -1,4 +1,19 @@
 #/bin/sh
+
+export LANG=en_US.utf8
+export LC_CTYPE="en_US.utf8"
+export LC_NUMERIC="en_US.utf8"
+export LC_TIME="en_US.utf8"
+export LC_COLLATE="en_US.utf8"
+export LC_MONETARY="en_US.utf8"
+export LC_MESSAGES="en_US.utf8"
+export LC_PAPER="en_US.utf8"
+export LC_NAME="en_US.utf8"
+export LC_ADDRESS="en_US.utf8"
+export LC_TELEPHONE="en_US.utf8"
+export LC_MEASUREMENT="en_US.utf8"
+export LC_IDENTIFICATION="en_US.utf8"
+
 GIT_DIR="/home/rupert/monogit"
 OUTPUT_DIR="/home/rupert/rank/rank-server/Output"
 GET_RANK_DIR="/home/rupert/rank/getRank/bin/Release/"
