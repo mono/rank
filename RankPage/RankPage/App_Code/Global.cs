298ea0ba76226bd7650bf0a1f235e6edf6cddf63
David Mulder <dmulder@novell.com>
2011-03-16 12:28:44 -0600

    Added App_Code

---
+++ RankPage/RankPage/App_Code/Global.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RankPage
+{
+	public static class Global
+	{
+		public static string Name {get; set;}
+		public static string Email {get; set;}
+		public static string Gravatar {get; set;}
+	}
+}
+
---
+++ RankPage/RankPage/App_Code/User_Data.cs
@@ -0,0 +1,11 @@
+using System;
+namespace RankPage
+{
+	public class User_Data
+	{
+		public User_Data ()
+		{
+		}
+	}
+}
+
