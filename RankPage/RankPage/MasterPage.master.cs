14294de36827ddd788564c18d4fd7ae2a6d8985f
David Mulder <dmulder@novell.com>
2011-03-17 13:17:13 -0600

    Working on getting UserInfo class working.

--- RankPage/RankPage/MasterPage.master.cs
+++ RankPage/RankPage/MasterPage.master.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Global;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-		Global.Global.Email = Request.Form["email"];
-		Global.Global.Name = Request.Form["name"];
+		
     }
 
 }
--- RankPage/RankPage/RankingPage.aspx.cs
+++ RankPage/RankPage/RankingPage.aspx.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Global;
 
 public partial class RankingPage : System.Web.UI.Page
 {
+	private UserInfo user;
+	
     protected void Page_Load(object sender, EventArgs e)
     {
-		currentUserName.Text = Global.Global.Name;
+		//user = new UserInfo(Request.Form["name"].ToString(), Request.Form["email"].ToString());
     }
 	
     protected void btnShowHow_Click(object sender, EventArgs e)
